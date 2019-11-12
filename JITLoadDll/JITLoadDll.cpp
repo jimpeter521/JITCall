9f2490b9bdff916a8a78825179719567488b5b44
Stephen Eckels <stevemk14ebr@gmailcom>
2019-11-11 20:08:39 -0500

    Add export listing, fix hardcoded include paths

--- JITLoadDll/JITLoadDll.cpp
+++ JITLoadDll/JITLoadDll.cpp
@@ -1,8 +1,12 @@
 #include "JITCall.hpp"
 #include "CommandParser.hpp"
 #include "MainWindow.hpp"
+#include "PEB.hpp"
 
 #include <stdio.h>
+#include <string>
+#include <vector>
+#include <cassert>
 
 int test(int i, float j) {
 	printf("One:%d Two:%f\n", i, j);
@@ -21,6 +25,36 @@ struct JITEnv {
 	std::unique_ptr<JITCall::Parameters> params;
 };
 
+std::vector<std::string> getExports(uint64_t moduleBase) {
+	assert(moduleBase != NULL);
+	if (moduleBase == NULL)
+		return std::vector<std::string>();
+
+	IMAGE_DOS_HEADER* pDos = (IMAGE_DOS_HEADER*)moduleBase;
+	IMAGE_NT_HEADERS* pNT = RVA2VA(IMAGE_NT_HEADERS*, moduleBase, pDos->e_lfanew);
+	IMAGE_DATA_DIRECTORY* pDataDir = (IMAGE_DATA_DIRECTORY*)pNT->OptionalHeader.DataDirectory;
+
+	if (pDataDir[IMAGE_DIRECTORY_ENTRY_EXPORT].VirtualAddress == NULL) {
+		ErrorLog::singleton().push("PEs without export tables are unsupported", ErrorLevel::SEV);
+		return std::vector<std::string>();
+	}
+
+	IMAGE_EXPORT_DIRECTORY* pExports = RVA2VA(IMAGE_EXPORT_DIRECTORY*, moduleBase, pDataDir[IMAGE_DIRECTORY_ENTRY_EXPORT].VirtualAddress);
+
+	uint32_t* pAddressOfFunctions = RVA2VA(uint32_t*, moduleBase, pExports->AddressOfFunctions);
+	uint32_t* pAddressOfNames = RVA2VA(uint32_t*, moduleBase, pExports->AddressOfNames);
+	uint16_t* pAddressOfNameOrdinals = RVA2VA(uint16_t*, moduleBase, pExports->AddressOfNameOrdinals);
+
+	std::vector<std::string> exports;
+	exports.reserve(pExports->NumberOfNames);
+	for (uint32_t i = 0; i < pExports->NumberOfNames; i++)
+	{
+		char* exportName = RVA2VA(char*, moduleBase, pAddressOfNames[i]);
+		exports.push_back(exportName);
+	}
+	return exports;
+}
+
 int main()
 {
 	Command cmdParser(GetCommandLineA());
@@ -35,6 +69,13 @@ int main()
 
 		// actally load
 		loadedModule = LoadLibraryA(dllPath.c_str());
+
+		std::vector<std::string> exports = getExports((uint64_t)loadedModule);
+		std::cout << "--Export Table Start--" << std::endl;
+		for (auto&& exp : exports) {
+			std::cout << exp << std::endl;
+		}
+		std::cout << "--Export Table End--" << std::endl;
 	});
 
 	window.OnNewFunction([&](const std::vector<FunctionEditor::State::ParamState>& paramStates, const char* retType, const char* exportName) {
